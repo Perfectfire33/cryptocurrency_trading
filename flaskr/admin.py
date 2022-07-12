de60e0d0198edb3aae9f0f9ee5487dafb4bb4024
Perfectfire33 <perfectfireiii@gmail.com>
2022-07-12 19:24:45 -0400

    Got Google Sheet API test implemented
    Also got Bittrex markets API test implemented

--- flaskr/admin.py
+++ flaskr/admin.py
@@ -1,9 +1,26 @@
+from __future__ import print_function
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
 from flaskr.auth import login_required
 from flaskr.db import get_db
 from werkzeug.security import generate_password_hash, check_password_hash
+import requests
+
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
+SAMPLE_RANGE_NAME = 'Class Data!A2:E'
 
 bp = Blueprint('admin', __name__)
 
@@ -12,6 +29,91 @@ bp = Blueprint('admin', __name__)
 @login_required
 def index():
     return render_template('admin/admin.html')
+
+
+@bp.route('/admin/get_data')
+@login_required
+def get_data():
+    api_url = "https://jsonplaceholder.typicode.com/todos/1"
+    response = requests.get(api_url)
+    print(response.json())
+    print(response.status_code)
+    print(response.headers["Content-Type"])
+    return render_template('admin/admin.html')
+
+@bp.route('/admin/post_data')
+@login_required
+def post_data():
+    api_url = "https://jsonplaceholder.typicode.com/todos"
+    todo = {"userId": 1, "title": "Buy milk", "completed": False}
+    response = requests.post(api_url, json=todo)
+    print(response.json())
+    print(response.status_code)
+    print(response.headers["Content-Type"])
+    return render_template('admin/admin.html')
+
+
+@bp.route('/admin/sheet_api_test')
+@login_required
+def sheet_api_test():
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    dirname = os.path.dirname(__file__)
+    credentials_file = os.path.join(dirname, 'credentials.json')
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                credentials_file, SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('sheets', 'v4', credentials=creds)
+
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                    range=SAMPLE_RANGE_NAME).execute()
+        values = result.get('values', [])
+
+        if not values:
+            print('No data found.')
+            return
+
+        print('Name, Major:')
+        for row in values:
+            # Print columns A and E, which correspond to indices 0 and 4.
+            print('%s, %s' % (row[0], row[4]))
+    except HttpError as err:
+        print(err)
+
+    return render_template('admin/admin.html')
+
+
+@bp.route('/admin/get_bittrex_markets')
+@login_required
+def get_bittrex_markets():
+    api_url = "https://api.bittrex.com/v3/markets"
+    response = requests.get(api_url)
+    print(response.json())
+    print(response.status_code)
+    print(response.headers["Content-Type"])
+    return render_template('admin/admin.html')
+
+
+
 
 @bp.route('/users_list')
 @login_required
